6b0f9f14ffc1c34e9ea86c4b3615b8a5a94d40a4
penalozas875 <penalozas875@gmail.com>
2025-12-03 23:13:36 -0500

    error

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost", "root", "12345", "crud_php");
+$conexion = new mysqli("localhost", "root", "nicolas2003605", "crud_php");
 if ($conexion->connect_error) {
     die("Error: " . $conexion->connect_error);
 }
